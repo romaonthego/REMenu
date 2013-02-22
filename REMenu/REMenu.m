67d81cddf84bec5c4885f058876cfe02171383dc
Roman Efimov <romaonthego@gmail.com>
2013-02-22 10:19:58 -0600

    Remove shadow radius from corner radius property

--- REMenu/REMenu.m
+++ REMenu/REMenu.m
@@ -170,7 +170,6 @@
 {
     _cornerRadius = cornerRadius;
     _menuView.layer.cornerRadius = cornerRadius;
-    _menuWrapperView.layer.shadowRadius = cornerRadius;
 }
 
 - (void)setShadowColor:(UIColor *)shadowColor
