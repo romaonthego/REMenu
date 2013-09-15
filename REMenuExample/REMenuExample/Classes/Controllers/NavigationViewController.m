7829d23996cff8ac663782e57e710631dffb99cc
Roman Efimov <romaonthego@gmail.com>
2013-09-15 09:12:46 -0500

    Update readme, project architecture

--- REMenuExample/REMenuExample/Classes/Controllers/NavigationViewController.m
+++ REMenuExample/REMenuExample/Classes/Controllers/NavigationViewController.m
@@ -108,6 +108,7 @@
         self.menu.shadowOffset = CGSizeMake(0, 1);
         self.menu.shadowOpacity = 1;
     }
+    self.menu.appearsBeyondNavigationBar = YES; // Affects only iOS 7
     self.menu.imageOffset = CGSizeMake(5, -1);
     self.menu.waitUntilAnimationIsComplete = NO;
     self.menu.badgeLabelConfigurationBlock = ^(UILabel *badgeLabel, REMenuItem *item) {
