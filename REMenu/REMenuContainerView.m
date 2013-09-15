5f5d06414ce03eb97719136ad04fd74f266492e5
Roman Efimov <romaonthego@gmail.com>
2013-09-15 09:03:42 -0500

    Fix offset in iPad landscape mode

--- REMenu/REMenuContainerView.m
+++ REMenu/REMenuContainerView.m
@@ -33,15 +33,17 @@
     [super layoutSubviews];
     UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
     
+    CGFloat landscapeOffset = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ? 32.0 : 44.0;
+    
     if (self.navigationBar && !self.appearsBeyondNavigationBar) {
         CGRect frame = self.frame;
-        frame.origin.y = self.navigationBar.frame.origin.y + (UIDeviceOrientationIsPortrait(orientation) ? 44.0 : 32.0);
+        frame.origin.y = self.navigationBar.frame.origin.y + (UIDeviceOrientationIsPortrait(orientation) ? 44.0 : landscapeOffset);
         self.frame = frame;
     }
     
     if (self.appearsBeyondNavigationBar) {
         CGRect frame = self.frame;
-        frame.origin.y = (UIDeviceOrientationIsPortrait(orientation) ? 44.0 : 32.0) - 44;
+        frame.origin.y = (UIDeviceOrientationIsPortrait(orientation) ? 44.0 : landscapeOffset) - 44;
         self.frame = frame;
     }
 }
