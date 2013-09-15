1bc367de671e4fb9bbcc6e8ea4b4f93b2fbbb9a4
Roman Efimov <romaonthego@gmail.com>
2013-09-14 21:00:14 -0500

    Move REUIKitIsFlatMode into its own file, fix method redefenition

--- REMenu/REMenu.h
+++ REMenu/REMenu.h
@@ -25,6 +25,7 @@
 
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
+#import "RECommonFunctions.h"
 #import "REMenuItem.h"
 #import "REMenuContainerView.h"
 
@@ -34,24 +35,6 @@ typedef NS_ENUM(NSInteger, REMenuImageAlignment) {
     REMenuImageAlignmentLeft,
     REMenuImageAlignmentRight
 };
-
-static BOOL REUIKitIsFlatMode()
-{
-    static BOOL isUIKitFlatMode = NO;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        if (floor(NSFoundationVersionNumber) > 993.0) {
-            // If your app is running in legacy mode, tintColor will be nil - else it must be set to some color.
-            if (UIApplication.sharedApplication.keyWindow) {
-                isUIKitFlatMode = [UIApplication.sharedApplication.delegate.window performSelector:@selector(tintColor)] != nil;
-            } else {
-                // Possible that we're called early on (e.g. when used in a Storyboard). Adapt and use a temporary window.
-                isUIKitFlatMode = [[UIWindow new] performSelector:@selector(tintColor)] != nil;
-            }
-        }
-    });
-    return isUIKitFlatMode;
-}
 
 @interface REMenu : NSObject 
 
