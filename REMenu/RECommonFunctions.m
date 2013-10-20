7c4227c583ee2a63cbdf68f42747956244bd3703
Shigeaki Matsumura <matsu911@gmail.com>
2013-10-20 17:18:21 +0900

    avoid crash in case of no 'window' property in app delegate

--- REMenu/RECommonFunctions.m
+++ REMenu/RECommonFunctions.m
@@ -32,7 +32,8 @@ BOOL REUIKitIsFlatMode()
     dispatch_once(&onceToken, ^{
         if (floor(NSFoundationVersionNumber) > 993.0) {
             // If your app is running in legacy mode, tintColor will be nil - else it must be set to some color.
-            if (UIApplication.sharedApplication.keyWindow) {
+            if (UIApplication.sharedApplication.keyWindow &&
+                [UIApplication.sharedApplication.delegate respondsToSelector:@selector(window)]) {
                 isUIKitFlatMode = [UIApplication.sharedApplication.delegate.window performSelector:@selector(tintColor)] != nil;
             } else {
                 // Possible that we're called early on (e.g. when used in a Storyboard). Adapt and use a temporary window.
